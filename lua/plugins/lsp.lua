1b220cd8913c77608816f97fdb83e062776df4da
Duncan Snider <duncan@masv.io>
2024-03-01 13:09:50 -0500

    Rebind accept_completion from return to ctrl+y

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,8 +32,6 @@ local servers = {
       })
     end,
   },
-
-  jsonls = {},
 }
 
 return {
@@ -192,7 +190,7 @@ return {
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete {},
-          ["<CR>"] = cmp.mapping.confirm {
+          ["<C-y>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           },
