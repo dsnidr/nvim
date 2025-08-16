1f3d6b8ccf73e8e3c667accd2c37be9249e23adb
dsnidr <duncan@dmas.dev>
2025-08-16 11:25:27 -0400

    Add tailwind support, OCS52

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,30 @@
 
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+function no_paste(_)
+  return function(_) end
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = no_paste("+"), -- Pasting disabled
+    ["*"] = no_paste("*"), -- Pasting disabled
+  },
+}
+
+vim.keymap.set("n", "<leader>gog", function()
+  local result = vim.fn.system("go generate ./...")
+  local exit_code = vim.v.shell_error
+
+  if exit_code == 0 then
+    vim.notify("Generation complete", vim.log.levels.INFO)
+  else
+    vim.notify("Generation failed:\n" .. result, vim.log.levels.ERROR)
+  end
+end, { desc = "Run go generate", silent = true })
